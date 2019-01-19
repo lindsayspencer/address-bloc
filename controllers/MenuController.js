20a2cc88bfc1760cac070904ecfba891cbf96e1e
Lindsay Spencer <43455549+lindsayspencer@users.noreply.github.com>
2019-01-19 15:54:59 -0600

    completed checkpoint: prompt and menu

---
+++ controllers/MenuController.js
@@ -0,0 +1,48 @@
+// a JS class with methods to handle receiving requests from the user
+const inquirer = require("inquirer");
+
+module.exports = class MenuController {
+  constructor() {
+    this.mainMenuQuestions = [
+      {
+        type: "list",
+        name: "mainMenuChoice",
+        message: "Please choose from an option below: ",
+        choices: ["Add new contact", "Exit"]
+      }
+    ];
+    this.contacts = [];
+  }
+  clear() {
+    console.log("\x1Bc");
+  }
+  main() {
+    console.log("Welcome to AddressBloc!");
+    inquirer
+      .prompt(this.mainMenuQuestions)
+      .then(response => {
+        switch (response.mainMenuChoice) {
+          case "Add new contact":
+            this.addContact();
+            break;
+          case "Exit":
+            this.exit();
+          default:
+            console.log("Invalid input");
+            this.main();
+        }
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  }
+  exit() {
+    console.log("Thanks for using AddressBloc!");
+    process.exit();
+  }
+  addContact() {
+    this.clear();
+    console.log("addContact called");
+    this.main();
+  }
+};
